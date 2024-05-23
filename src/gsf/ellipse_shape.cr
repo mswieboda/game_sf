b24d36d42161ef42311c02e7b6129560b7e3d408
Matt Swieboda <mswieboda@users.noreply.github.com>
2024-05-22 23:04:52 -0500

    fixes some EllipseShape issues

--- src/gsf/ellipse_shape.cr
+++ src/gsf/ellipse_shape.cr
@@ -3,14 +3,27 @@ module GSF
     getter radius : SF::Vector2f
     getter point_count : Int32
 
-    def initialize(@radius : SF::Vector2f = SF::Vector2f.new, @point_count : Int = 30)
+    def initialize(
+      radius : SF::Vector2f | Tuple(Number, Number) = SF::Vector2f.new,
+      @point_count : Int = 30
+    )
+      if radius.is_a?(SF::Vector2f)
+        @radius = radius.as(SF::Vector2f)
+      else
+        @radius = SF::Vector2f.new(radius[0].to_f32, radius[1].to_f32)
+      end
+
       super()
 
       update
     end
 
-    def radius=(radius : SF::Vector2f)
-      @radius
+    def radius=(radius : SF::Vector2f | Tuple(Number, Number))
+      if radius.is_a?(SF::Vector2f)
+        @radius = radius.as(SF::Vector2f)
+      else
+        @radius = SF::Vector2f.new(radius[0], radius[1])
+      end
 
       update
     end
