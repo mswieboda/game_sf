6b05154a6953a8c5f4360cf757549343272cbc73
Matt Swieboda <mswieboda@users.noreply.github.com>
2024-05-22 22:32:52 -0500

    fix bump binary

--- src/bump.cr
+++ src/bump.cr
@@ -6,27 +6,40 @@ module GSF
   VersionFile = "src/version.cr"
 
   class Bump
-    @version : String = GSF::VERSION
+    @prev_version = ""
+    @version = ""
+    @action = ""
 
-    def parse_version
+    def get_current_version
+      file = File.read(ShardFile)
+      find = "\nversion: "
+      start_index = (file.index(find) || 0) + find.size - 1
+      end_index = file.index("\n", start_index)
+      file[start_index..end_index].strip
+    end
+
+    def new_version
       arg = ARGV.empty? ? "patch" : ARGV.first
+      @action = arg
 
       if arg == "patch"
-        parts = @version.split(".")
+        parts = @prev_version.split(".")
         parts[2] = (parts[2].to_i + 1).to_s
         parts.join(".")
       elsif arg == "minor"
-        parts = @version.split(".")
+        parts = @prev_version.split(".")
         parts[1] = (parts[1].to_i + 1).to_s
         parts[2] = "0"
         parts.join(".")
       elsif arg == "major"
-        parts = @version.split(".")
+        parts = @prev_version.split(".")
         parts[0] = (parts[0].to_i + 1).to_s
         parts[1] = "0"
         parts[2] = "0"
         parts.join(".")
       else
+        @action = "version"
+
         arg
       end
     end
@@ -37,14 +50,14 @@ module GSF
       # shard.yml
       replace_text(
         file_name: ShardFile,
-        find: "name: game_sf\nversion: #{GSF::VERSION}",
+        find: "name: game_sf\nversion: #{@prev_version}",
         replace: "name: game_sf\nversion: #{@version}"
       )
 
       # version.cr
       replace_text(
         file_name: VersionFile,
-        find: "VERSION = \"#{GSF::VERSION}\"",
+        find: "VERSION = \"#{@prev_version}\"",
         replace: "VERSION = \"#{@version}\""
       )
     end
@@ -64,9 +77,10 @@ module GSF
     end
 
     def run
-      @version = parse_version
+      @prev_version = get_current_version
+      @version = new_version
 
-      puts "v#{@version}"
+      puts "#{@action} v#{@prev_version} -> v#{@version}"
 
       replace_versions
       git_commit
